98f6bc14dc774ca999b824828cf59219ebdf6914
Your Name <vinicius_benner@estudante.sesisenai.org.br>
2021-12-22 21:03:40 -0300

    f
    ff

--- src/app/conteudo/produtos/produto/produto.component.ts
+++ src/app/conteudo/produtos/produto/produto.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-produto',
   templateUrl: './produto.component.html',
   styleUrls: ['./produto.component.css']
 })
 export class ProdutoComponent implements OnInit {
-
-  constructor() { }
-
+  idProduto;
+  constructor(private route: ActivatedRoute,
+    private router: Router) {
+    this.idProduto = route.snapshot.paramMap.get('idProduto')
+  }
+console(idProduto){
+  alert(idProduto)
+}
   ngOnInit() {
   }
-
 }
